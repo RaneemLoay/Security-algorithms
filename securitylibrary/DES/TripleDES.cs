e332e0f18ecf6cc0514b8ac6f48730a3043cceed
Raneem Loay <103434226+RaneemLoay@users.noreply.github.com>
2023-06-06 16:20:30 +0300

    Update TripleDES.cs

--- securitylibrary/DES/TripleDES.cs
+++ securitylibrary/DES/TripleDES.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SecurityLibrary.DES;
 namespace SecurityLibrary.DES
 {
     /// <summary>
@@ -13,18 +13,26 @@ namespace SecurityLibrary.DES
     {
         public string Decrypt(string cipherText, List<string> key)
         {
-            throw new NotImplementedException();
+            DES des = new DES();
+            string res = des.Decrypt(cipherText, key[0]);
+            res = des.Encrypt(res, key[1]);
+            res = des.Decrypt(res, key[1]);
+            return res;
         }
 
         public string Encrypt(string plainText, List<string> key)
         {
-            throw new NotImplementedException();
+            DES des = new DES();
+            string res = des.Encrypt(plainText, key[0]);
+            res = des.Decrypt(res, key[1]);
+            res = des.Encrypt(res, key[1]);
+            return res;
         }
 
         public List<string> Analyse(string plainText, string cipherText)
         {
             throw new NotSupportedException();
-        }
+        }
 
-    }
+    }
 }
